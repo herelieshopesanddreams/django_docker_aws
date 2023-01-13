115c7eb2e70c5a829955137ede0b81991ea3d7a1
wei <eljayyes@gmail.com>
2023-01-13 01:55:09 -0800

    first commit

--- app/nc_tutorials/settings.py
+++ app/nc_tutorials/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from decouple import config
 import os
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,7 +126,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+else:
+    STATIC_ROOT = [os.path.join(BASE_DIR, "static")]
 STATIC_URL = '/static/'
 
 # Default primary key field type
